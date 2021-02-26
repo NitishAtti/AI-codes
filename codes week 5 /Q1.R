ceb4a4e12a168a2e2e413a1d93d681542f8ee19b
Nitish Atti <68691627+NitishAtti@users.noreply.github.com>
2021-02-26 11:09:25 +0530

    Create Q1.R

---
+++ codes week 5 /Q1.R
@@ -0,0 +1,6 @@
+library(bnlearn)
+course.grades<-read.table("2020_bn_nb_data.txt",head=TRUE)
+course.grades<-lapply(course.grades,as.factor)
+course.grades<-data.frame(course.grades)
+course.grades.net<-hc(course.grades,score = "k2")
+plot(course.grades.net)
